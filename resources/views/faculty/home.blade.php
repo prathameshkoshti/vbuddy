6bdf64b3b562dbf119435973509377028cf02e54
deva <parabdevendra999@gmail.com>
2018-01-22 22:47:03 +0530

    Faculty home page card layout

--- resources/views/faculty/home.blade.php
+++ resources/views/faculty/home.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.faculty_layout')
 
 @section('content')
+
+    <p>Dashboard <br>You are loged in as faculty</p><br>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="card">
-                    <div class="card-content">
-                        <i class="fa fa-bullhorn fa-4x"></i><br><br>
-                        <span class="card-title">Faculty Announcements</span>
+
+            <div class="col-md-3" style="background-color: white;">
+                <div class="card" style="">
+                    <div class="card-block" >
+                        <h3 class="card-title" style="text-align:center;color: black">Designation:Faculty </h3>
+                        <h4 class="card-text" style="color: black">Name:Kiran Wadkar <br> <br>Mail-id:kiranwadkar9@gmail.com <br>  <br>Contact No: 969782539</h4>
                     </div>
                 </div>
             </div>
+
+            <div class=" col-md-offset-1 col-md-2" style="background-color: white;">
+                <div class="card">
+                    <div class="card-block" style="background-color:#E00000">
+                        <h1 class="card-title" style="background-color: orangered;text-align: center;font-size: 8em;color: white;padding: 10px"><i class="fa fa-users" aria-hidden="true"></i></h1>
+                        <h1 class="card-text"  style="background-color:#E00000;color: black;text-align: center">Attendence</h1>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
     </div>
 @stop
