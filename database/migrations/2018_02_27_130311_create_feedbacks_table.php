f567239bffd2b10f424a14d7bc0b328d82524523
prathameshkoshti <prathu48@gmail.com>
2018-02-27 18:35:21 +0530

    minor change

--- database/migrations/2018_02_27_130311_create_feedbacks_table.php
+++ database/migrations/2018_02_27_130311_create_feedbacks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableFeedbackAnswers extends Migration
+class CreateFeedbacksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -102,6 +102,6 @@ class CreateTableFeedbackAnswers extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('feedback_answers');
+        Schema::dropIfExists('feedbacks');
     }
 }
