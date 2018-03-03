0afd37f273b75307e92910afff5d5b7fcd4e31e9
prathameshkoshti <prathu48@gmail.com>
2018-03-03 12:14:41 +0530

    minor chnage in  welcome and home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,21 +8,24 @@
 
 @section('content')
 {!! Charts::styles() !!}
-    <div class="row">
-        <div class="col-md-8">
-            {!! $events_chart->html() !!}
-        </div>
-        <div class="col-md-4">
-            <div class="alert alert-info" role="alert">
-                <h3>{{$event->name}}</h3>
-                <h4>{{$event->details}}</h4>
-                <p>Issued-By:&nbsp;{{$user_event->name}}</p>
-                <p>{{$event->date}}</p><br>
-                <p>Registrations done: {{$event->event_registration_count}}</p>
-                <a style="float:right;" class="alert-link" href="/admin/events">more..</a><br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8">
+                <br>
+                {!! $events_chart->html() !!}
+                <br>
             </div>
-        </div>
-        <div class="col-md-4">
+            <div class="col-md-4">
+                <div class="alert alert-info" role="alert">
+                    <h3>{{$event->name}}</h3>
+                    <h4>{{$event->details}}</h4>
+                    <p>Issued-By:&nbsp;{{$user_event->name}}</p>
+                    <p>{{$event->date}}</p><br>
+                    <p>Registrations done: {{$event->event_registration_count}}</p>
+                    <a style="float:right;" class="alert-link" href="/admin/events">more..</a><br>
+                </div>
+            </div>
+            <div class="col-md-4">
                 <div class="alert alert-warning" role="alert">
                     <h3>{{$placement->head}}</h3>
                     <h4>{{$placement->body}}</h4>
@@ -31,10 +34,10 @@
                     <a style="float:right;" class="alert-link" href="/admin/events">more..</a><br>
                 </div>
             </div>  
-    </div>    
-    <div class="row">
-    </div>
-    
+        </div>
+        <div class="row">
+        </div>
+    </div>        
     {!! Charts::scripts() !!}
     {!! $events_chart->script() !!}
 @stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,22 +76,13 @@
                         @endif    
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    V-Buddy
                 </div>
             </div>
         </div>
