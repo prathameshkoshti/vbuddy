a1addca5f3705b8ea5e1e936ea26159ff2688d82
deva <parabdevendra999@gmail.com>
2018-03-08 12:38:18 +0530

    addingsearcch box in event registration

--- resources/views/admin/event_registrations/view.blade.php
+++ resources/views/admin/event_registrations/view.blade.php
@@ -5,15 +5,56 @@
 @section('content_header')
     <h1 style="text-align:center">Events Registrations</h1>
 @stop
+
+<script>
+    function myFunction() {
+        var input, filter, table, tr, td1, td2, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td1 = tr[i].getElementsByTagName("td")[1];
+            td2 = tr[i].getElementsByTagName("td")[2];
+
+            if (td1 || td2) {
+                if (td1.innerHTML.indexOf(filter) > -1 || td2.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+</script>
+
+
+<style>
+
+</style>
+
+
+
 @section('content')
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-8 col-md-offset-2">
             <div class="table-responsive">
-                <table class="table table-hover table-borderless">
-                    <tr>
-                        <td align="right" colspan=5><b>Registrations Count:{{$count->event_registration_count}}</b>  </td>
+                <table id="myTable" class="table table-hover table-borderless">
+                    <tr class="header">
+                        <th colspan="2"></th>
+                        <th colspan="2">
+                            <div class="input-group">
+
+                                <input type="text"  id="myInput" class="search" onkeyup="myFunction()" placeholder="Search for Name or Roll No...." title="Type in a name" size="100" style=" border-radius: 100px !important;">
+
+                            </div>
+                        </th>
+
+                        <th><b>Registrations Count:{{$count->event_registration_count}}</b></th>
+
+
                     </tr>
-                    <tr>
+                    <tr class="header">
                         <th>ID</th>
                         <th>Roll No.</th>
                         <th>Name</th>
